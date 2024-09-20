e9a3cbb305e394921bd0e288276716c2c99d18a0
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-09-21 01:12:37 +0500

    Cursor

---
+++ src/components/common/Cursor.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+import mouseEffect from "@/js/mouseEffect";
+
+const Cursor = () => {
+  React.useEffect(() => {
+    mouseEffect();
+  }, []);
+  return (
+    <>
+      <div className="mouse-cursor cursor-outer"></div>
+      <div className="mouse-cursor cursor-inner"></div>
+    </>
+  );
+};
+
+export default Cursor;
