12f1b12953295036b3e86d91e8209c0d321645e6
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-09-22 16:54:19 +0500

    Home Notifications

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Banner from "@/components/home/Banner";
 import Intro from "@/components/home/Intro";
+import Notification from "@/components/home/Notification";
 
 export default function HomePage() {
   return (
     <>
       <Banner />
       <Intro />
+      <Notification />
     </>
   );
 }
---
+++ src/data/notificationsData.ts
@@ -0,0 +1,64 @@
+// notificationsData.ts
+export const notificationsData = [
+  {
+    id: 1,
+    image: "/img/notifications/balloting-quota-for-5-marla-and-7-marla.jpeg",
+    alt: "balloting-quota-for-5-marla-and-7-marla",
+  },
+  {
+    id: 2,
+    image: "/img/notifications/english-notification.jpeg",
+    alt: "english-notification",
+  },
+  {
+    id: 3,
+    image:
+      "/img/notifications/launch-of-merging-vouchers-and-ledger-closing.jpeg",
+    alt: "launch-of-merging-vouchers-and-ledger-closing",
+  },
+  {
+    id: 4,
+    image: "/img/notifications/merging-policy-2023.jpeg",
+    alt: "merging-policy-2023",
+  },
+  {
+    id: 5,
+    image: "/img/notifications/merging-policy-2023-1.jpeg",
+    alt: "merging-policy-2023-1",
+  },
+  {
+    id: 6,
+    image: "/img/notifications/open-file-verification.jpeg",
+    alt: "open-file-verification",
+  },
+  {
+    id: 7,
+    image: "/img/notifications/price-plan-revision.jpeg",
+    alt: "price-plan-revision",
+  },
+  {
+    id: 8,
+    image: "/img/notifications/price-plan-revision-1.jpeg",
+    alt: "price-plan-revision-1",
+  },
+  {
+    id: 9,
+    image: "/img/notifications/price-plan-revision-2.jpeg",
+    alt: "price-plan-revision-2",
+  },
+  {
+    id: 10,
+    image: "/img/notifications/reminder.jpeg",
+    alt: "reminder",
+  },
+  {
+    id: 11,
+    image: "/img/notifications/urdu-notification.jpeg",
+    alt: "urdu-notification",
+  },
+  {
+    id: 12,
+    image: "/img/notifications/urdu-notification-1.jpeg",
+    alt: "urdu-notification-1",
+  },
+];
